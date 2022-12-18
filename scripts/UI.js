f256a24fdccc6e26edb57add564f02979b64c484
krishna8464 <yendavamshikrishna123@gmail.com>
2022-12-19 01:39:24 +0530

    final submision done

--- scripts/UI.js
+++ scripts/UI.js
@@ -5,3 +5,62 @@ function Logout(){
     sessionStorage.clear()
     window.location.href="index.html"
 }
+
+//////don't touch the above code
+function individual(){
+    let obj = {
+        planname : "individual",
+        planamount : "$14"
+    }
+    sessionStorage.setItem("plan",JSON.stringify(obj))
+    setTimeout(() => {
+        window.location.href="setting.html"
+    }, 1000);
+    
+}
+
+function team(){
+    let obj = {
+        planname : "Team",
+        planamount : "$34"
+    }
+    sessionStorage.setItem("plan",JSON.stringify(obj))
+    setTimeout(() => {
+        window.location.href="setting.html"
+    }, 1000);
+}
+
+function business(){
+    let obj = {
+        planname : "Business",
+        planamount : "$64"
+    }
+    sessionStorage.setItem("plan",JSON.stringify(obj))
+    setTimeout(() => {
+        window.location.href="setting.html"
+    }, 1000);
+}
+function Premium(){
+    let obj = {
+        planname : "Premium",
+        planamount : "$129"
+    }
+    sessionStorage.setItem("plan",JSON.stringify(obj))
+    setTimeout(() => {
+        window.location.href="setting.html"
+    }, 1000);
+}
+function enterprise(){
+    let obj = {
+        planname : "Enterprise",
+        planamount : "$399"
+    }
+    sessionStorage.setItem("plan",JSON.stringify(obj))
+    setTimeout(() => {
+        window.location.href="setting.html"
+    }, 1000);
+}
+
+function moveit(){
+    window.location.href="inbox.html"
+}
